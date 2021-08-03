f095db595723b23847fb231069595c27468a2724
Frank Yan <franklyan94@gmail.com>
2021-08-03 08:51:37 -0400

    Update ArmstrongNumber.cs with comments

--- C#-1-Demo/Demo/Exercise4/ArmstrongNumber.cs
+++ C#-1-Demo/Demo/Exercise4/ArmstrongNumber.cs
@@ -6,9 +6,9 @@ namespace Exercise4
 {
     class ArmstrongNumber
     {
-        public int lowerBound = 0;
-        public int upperBound = 1000;
-        public ArmstrongNumber(int lower, int upper)
+        public int lowerBound = 0;                      // default value - lower bound of input pair
+        public int upperBound = 1000;                   // default value - upper bound of input pair
+        public ArmstrongNumber(int lower, int upper)    // class instance init
         {
             lowerBound = lower;
             upperBound = upper;
@@ -16,26 +16,26 @@ namespace Exercise4
 
         public int[] AllResults()
         {
-            List<int> aNums = new List<int>();
+            List<int> aNums = new List<int>();          // record qualified numbers
             for (int i = lowerBound; i < upperBound; i++)
             {
-                if (CubeSum(i) == i) aNums.Add(i);
+                if (CubeSum(i) == i) aNums.Add(i);      // qualify
             }
-            int[] results = aNums.ToArray();
+            int[] results = aNums.ToArray();            
             return results;
         }
 
-        public int CubeSum(int num)
+        public int CubeSum(int num)                     // helper method to qualify armstrong numbers
         {
-            int digits = 0;
-            int temp = num;
-            while (temp != 0)
+            int digits = 0;                             // variable to store digits of the number - exponent value
+            int temp = num;                             // helper variable to traverse each digit
+            while (temp != 0)                           // calculate exponent value
             {
                 temp /= 10;
                 digits++;
             }
             int cubeSum = 0;
-            for (; num != 0; num /= 10)
+            for (; num != 0; num /= 10)                 // calculate 
             {
                 cubeSum += Convert.ToInt32(Math.Pow((num % 10), digits));
             }
