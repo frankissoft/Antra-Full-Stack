e40585def954584d4c5fe413803835231f227cd4
Runge Yan <franklyan94@gmail.com>
2021-08-09 21:07:52 -0400

    Upload and update MovieShop
    Work after day 1

---
+++ MovieShop/MovieShopMVC/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShopMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginRequestModel model)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(UserRegisterRequestModel model)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MovieShop/MovieShopMVC/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieShopMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShopMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // private readonly ILogger<HomeController> _logger;
+
+        // public HomeController(ILogger<HomeController> logger)
+        private MovieService _movieService;
+        public HomeController()
+        {
+            // _logger = logger;
+            _movieService = new MovieService();
+        }
+
+        public IActionResult Index()
+        {
+            // get popular movies and diplay on the view
+            var movies = _movieService.GetTopRevenueMovies();
+            // 2 ways to pass data from controller to view
+            // 1) Strongly typed models***
+            // 2) ViewBag
+            // 3) ViewData
+            // localhost:5000/movies/details/2
+            ViewBag.PageTitle = "Top Revenue Movies";
+            ViewData["TotalMovies"] = movies.Count();
+            return View(movies);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
