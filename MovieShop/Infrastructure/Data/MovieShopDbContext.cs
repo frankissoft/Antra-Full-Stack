cfdb4f1ab574f7604a7efb7916b8179c24b58010
Runge Yan <franklyan94@gmail.com>
2021-08-12 08:59:10 -0400

    Created Database

---
+++ MovieShop/Infrastructure/Data/MovieShopDbContext.cs
@@ -0,0 +1,180 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class MovieShopDbContext : DbContext
+    {
+        // DbSets -> tables
+        public MovieShopDbContext(DbContextOptions<MovieShopDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<MovieGenre> MovieGenres { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Trailer> Trailers { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<MovieCast> MovieCasts { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Favorite> Favorite { get; set; }
+        public DbSet<Purchase> Purchase { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<UserRole> UserRole { get; set; }
+        public DbSet<Crew> Crew { get; set; }
+        public DbSet<MovieCrew> MovieCrew { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Movie>().HasMany(m => m.Genres).WithMany(g => g.Movies)
+            //    .UsingEntity<Dictionary<string, object>>("MovieGenre",
+            //    m => m.HasOne<Genre>().WithMany().HasForeignKey("GenreID"),
+            //    m => m.HasOne<Movie>().WithMany().HasForeignKey("MovieID"));
+
+            modelBuilder.Entity<Movie>(ConfigureMovie);
+            modelBuilder.Entity<MovieGenre>(ConfigureMovieGenre);
+            modelBuilder.Entity<Trailer>(ConfigureTrailer);
+            modelBuilder.Entity<Cast>(ConfigureCast);
+            modelBuilder.Entity<MovieCast>(ConfigureMovieCast);
+
+            modelBuilder.Entity<User>(ConfigureUser);
+            modelBuilder.Entity<Favorite>(ConfigureFavorites);
+            modelBuilder.Entity<Review>(ConfigureReviews);
+            modelBuilder.Entity<Purchase>(ConfigurePurchases);
+
+            modelBuilder.Entity<Role>(ConfigureRoles);
+            modelBuilder.Entity<UserRole>(ConfigureUserRoles);
+
+            modelBuilder.Entity<Crew>(ConfigureCrews);
+            modelBuilder.Entity<MovieCrew>(ConfigureMovieCrews);
+        }
+        
+        private void ConfigureMovieGenre(EntityTypeBuilder<MovieGenre> builder)
+        {
+            builder.ToTable("MovieGenre");
+            builder.HasKey(mg => new { mg.MovieId, mg.GenreId });
+            builder.HasOne(mg => mg.Movie).WithMany(mg => mg.MovieGenres).HasForeignKey(mg => mg.MovieId);
+            builder.HasOne(mg => mg.Genre).WithMany(mg => mg.MovieGenres).HasForeignKey(mg => mg.GenreId);
+        }
+        private void ConfigureMovieCrews(EntityTypeBuilder<MovieCrew> builder)
+        {
+
+            builder.ToTable("MovieCrew");
+            builder.HasKey(mc => new { mc.MovieId, mc.CrewId, mc.Department, mc.Job });
+            builder.HasOne(mc => mc.Movie).WithMany(mc => mc.MovieCrews).HasForeignKey(mc => mc.MovieId);
+            builder.HasOne(mc => mc.Crew).WithMany(mc => mc.MovieCrews).HasForeignKey(mc => mc.CrewId);
+        }
+        private void ConfigureCrews(EntityTypeBuilder<Crew> builder)
+        {
+            builder.ToTable("Crew");
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Name).HasMaxLength(128);
+            builder.Property(c => c.Gender).HasMaxLength(4096);
+            builder.Property(c => c.TmdbUrl).HasMaxLength(4096);
+            builder.Property(c => c.ProfilePath).HasMaxLength(2048);
+        }
+        private void ConfigureRoles(EntityTypeBuilder<Role> builder)
+        {
+            builder.ToTable("Role");
+            builder.HasKey(r => r.Id);
+            builder.Property(r => r.Name).HasMaxLength(20);
+        }
+        private void ConfigureUserRoles(EntityTypeBuilder<UserRole> builder)
+        {
+            builder.ToTable("UserRole");
+            builder.HasKey(u => new { u.RoleId, u.UserId });
+            builder.HasOne(u => u.Role).WithMany(u => u.UserRoles).HasForeignKey(u => u.RoleId);
+            builder.HasOne(u => u.User).WithMany(u => u.UserRoles).HasForeignKey(u => u.UserId);
+        }
+        private void ConfigurePurchases(EntityTypeBuilder<Purchase> builder)
+        {
+            builder.ToTable("Purchase");
+            builder.HasKey(p => p.Id);
+            builder.HasOne(p => p.Movie).WithMany(p => p.Purchases).HasForeignKey(p => p.MovieId);
+            builder.HasOne(p => p.User).WithMany(p => p.Purchases).HasForeignKey(p => p.UserId);
+            builder.Property(p => p.PurchaseNumber);
+            builder.Property(p => p.TotalPrice).HasColumnType("decimal(18, 2)");
+            builder.Property(p => p.PurchaseDateTime).HasDefaultValueSql("getdate()");
+
+        }
+        private void ConfigureReviews(EntityTypeBuilder<Review> builder)
+        {
+            builder.ToTable("Review");
+            builder.HasKey(r => new { r.MovieId, r.UserId });
+            builder.Property(r => r.Rating).HasColumnType("decimal(3, 2)");
+            builder.Property(r => r.ReviewText).HasMaxLength(4096);
+            builder.HasOne(r => r.Movie).WithMany(r => r.Reviews).HasForeignKey(r => r.MovieId);
+            builder.HasOne(r => r.User).WithMany(r => r.Reviews).HasForeignKey(r => r.UserId);
+        }
+        private void ConfigureFavorites(EntityTypeBuilder<Favorite> builder)
+        {
+            builder.ToTable("Favorite");
+            builder.HasKey(f => f.Id);
+            builder.HasOne(f => f.Movie).WithMany(f => f.Favorites).HasForeignKey(f => f.MovieId);
+            builder.HasOne(f => f.User).WithMany(f => f.Favorites).HasForeignKey(f => f.UserId);
+        }
+        private void ConfigureUser(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.FirstName).HasMaxLength(128);
+            builder.Property(u => u.LastName).HasMaxLength(128); 
+            builder.Property(u => u.DateOfBirth).HasDefaultValueSql("getdate()");
+            builder.Property(u => u.Email).HasMaxLength(256);
+            builder.Property(u => u.HashedPassword).HasMaxLength(1024);
+            builder.Property(u => u.Salt).HasMaxLength(1024);
+            builder.Property(u => u.PhoneNumber).HasMaxLength(16);
+            builder.Property(u => u.TwoFactorEnabled);
+            builder.Property(u => u.LockoutEndDate).HasDefaultValueSql("getdate()");
+            builder.Property(u => u.LastLoginDateTime).HasDefaultValueSql("getdate()"); 
+            builder.Property(u => u.IsLocked);
+            builder.Property(u => u.AccessFailedCount);
+        }
+        private void ConfigureMovieCast(EntityTypeBuilder<MovieCast> builder)
+        {
+            builder.ToTable("MovieCast");
+            builder.HasKey(mc => new { mc.CastId, mc.MovieId, mc.Character });
+            builder.HasOne(mc => mc.Movie).WithMany(mc => mc.MovieCasts).HasForeignKey(mc => mc.MovieId);
+            builder.HasOne(mc => mc.Cast).WithMany(mc => mc.MovieCasts).HasForeignKey(mc => mc.CastId);
+        }
+        private void ConfigureCast(EntityTypeBuilder<Cast> builder)
+        {
+            builder.ToTable("Cast");
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Name).HasMaxLength(2048);
+            builder.Property(c => c.ProfilePath).HasMaxLength(2048);
+        }
+        private void ConfigureTrailer(EntityTypeBuilder<Trailer> builder)
+        {
+            builder.ToTable("Trailer");
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.TrailerUrl).HasMaxLength(2048);
+            builder.Property(t => t.Name).HasMaxLength(2048);
+        }
+        private void ConfigureMovie(EntityTypeBuilder<Movie> builder)
+        {
+            // Fluent API
+            builder.ToTable("Movie");
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.Title).HasMaxLength(256);
+            builder.Ignore(m => m.Rating);      // Not as a column in database
+            builder.Property(m => m.Overview).HasMaxLength(4096);
+            builder.Property(m => m.Tagline).HasMaxLength(512);
+            builder.Property(m => m.ImdbUrl).HasMaxLength(2084);
+            builder.Property(m => m.TmdbUrl).HasMaxLength(2084);
+            builder.Property(m => m.PosterUrl).HasMaxLength(2084);
+            builder.Property(m => m.BackdropUrl).HasMaxLength(2084);
+            builder.Property(m => m.OriginalLanguage).HasMaxLength(64);
+            builder.Property(m => m.Price).HasColumnType("decimal(5, 2)").HasDefaultValue(9.9m);
+            builder.Property(m => m.Budget).HasColumnType("decimal(18, 4)").HasDefaultValue(9.9m);
+            builder.Property(m => m.Revenue).HasColumnType("decimal(18, 4)").HasDefaultValue(9.9m);
+            builder.Property(m => m.CreatedDate).HasDefaultValueSql("getdate()");
+        }
+    }
+}
